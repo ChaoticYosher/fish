fde77beb9e671591543ac84a7def80197dae27d3
Yosher <5094164+ChaoticYosher@users.noreply.github.com>
2024-11-17 23:30:45 -0700

    wip add button to spawn window

---
+++ Scripts/Logic/FishGarden/fish_garden_button.gd
@@ -0,0 +1,20 @@
+extends Button
+
+@onready var window_scene = preload("res://Scenes/fish_garden.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_fish_garden_pressed() -> void:
+	var window = window_scene.instantiate()
+	add_child(window)
+	window.load_scene("res://Scenes/fish_garden.tscn", "Fish Garden")
+
+	get_tree().call_group("window_buttons", "release_focus")
